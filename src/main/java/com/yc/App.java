51dffa9b3dbd6f7fe473b8eed8fff05c17dd0d0e
ben <yinwencai@gmail.com>
2018-01-15 20:35:56 +0800

    添加jdk测试方法

--- src/main/java/com/yc/App.java
+++ src/main/java/com/yc/App.java
@@ -6,7 +6,22 @@ package com.yc;
 public class App {
     public static void main(String[] args) {
 
-        LambdaAssignment lambdaTest = new LambdaAssignment();
-        lambdaTest.assignment();
+//        LambdaAssignment lambdaTest = new LambdaAssignment();
+//        lambdaTest.assignment();
+
+        System.out.println("the inc value is: " + inc());
+    }
+
+    public static int inc() {
+        int x;
+        try{
+            x=1;
+            return x;
+        } catch( Exception e) {
+            x=2;
+            return x;
+        } finally{
+            x=3;
+        }
     }
 }
